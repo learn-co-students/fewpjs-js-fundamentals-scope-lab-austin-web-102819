f0e973b1bfc2f3d06bea6b5d64fc2bf68f89c47d
github username <github email address>
2019-12-11 11:42:36 -0500

    Done.

--- index.js
+++ index.js
@@ -3,16 +3,15 @@ let animal = "dog"
 function myAnimal() {
   return animal
 }
-
 function yourAnimal() {
   // How can we make sure that this function
   // and the above function both pass?
   // P.S.: You can't just hard-code 'cat' below
-  const animal = "cat"
+  const animal = "cat";
   return animal
 }
 
 function add2(n) {
-  const two = 2
+  const two = 2;
   return n + two
 }
